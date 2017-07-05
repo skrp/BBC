9fbb8b452f56dfb183a21652ae1c0b2dba69a92d
Prince of Archives <rich.tijerina@gmail.com>
2017-07-04 20:33:15 -0700

    Update LS.pl

--- LS.pl
+++ LS.pl
@@ -1,2 +1,10 @@
 #!/usr/local/bin/perl
+###################################
+# LS - massive archive file listing
 use strict; use warnings;
+
+opendir(my $dh, $dir);
+my @ls = readdir($dh);
+
+my $count = @ls;
+print "$count\n";
