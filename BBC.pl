10728d4082d93d74940da680abe4564aaafafab0
Prince of Archives <rich.tijerina@gmail.com>
2017-07-04 18:44:11 -0700

    Create BBC.pl

---
+++ BBC.pl
@@ -0,0 +1,11 @@
+##########################
+# BBC - binary block craft
+
+my $BLOCKSIZE = 10000;
+
+my $DATASIZE = 9968;
+my $ECBSIZE = 32;
+my @k;
+my $g = "@nam[$i] @pat[$i] @siz[$i] @enc[$i]";
+my $k = 
+
