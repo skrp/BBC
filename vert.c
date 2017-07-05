24bf674b1fbb30777cc774158693e76d6ab4bdf6
Prince of Archives <rich.tijerina@gmail.com>
2017-07-04 19:26:29 -0700

    Update vert.c

--- vert.c
+++ vert.c
@@ -8,3 +8,6 @@ char opath[100], npath[100];
 
 if ((ofp = open(opath, r)) < 0)
 { printf("fail open $s\n", opath); exit(1); }
+
+if ((nfp = open(npath, r)) < 0)
+{ printf("fail open $s\n", npath); exit(1); }
