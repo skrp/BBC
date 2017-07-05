d2eb2ff62f81b17ecd7ead7a53852cf96aaec350
Prince of Archives <rich.tijerina@gmail.com>
2017-07-04 19:30:08 -0700

    Update vert.c

--- vert.c
+++ vert.c
@@ -5,9 +5,14 @@
 
 FILE *ofp, *nfp;
 char opath[100], npath[100];
+char *set[];
 
 if ((ofp = open(opath, r)) < 0)
-{ printf("fail open $s\n", opath); exit(1); }
+  { printf("fail open $s\n", opath); exit(1); }
 
 if ((nfp = open(npath, r)) < 0)
-{ printf("fail open $s\n", npath); exit(1); }
+  { printf("fail open $s\n", npath); exit(1); }
+
+if ((fscanf(ofp, %x, &set[i])
+  { printf("fscanf fail %i", &set[i]); exit(1); }
+     
