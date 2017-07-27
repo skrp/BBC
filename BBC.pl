6a388038f26d65ce01631442446e3b013770e80f
Prince of Archives <rich.tijerina@gmail.com>
2017-07-26 17:53:27 -0700

    Update BBC.pl

--- BBC.pl
+++ BBC.pl
@@ -1,12 +1,23 @@
 #!/usr/local/bin/perl
 ##########################
-# BBC - binary block craft
-
-my $BLOCKSIZE = 10000;
-
-my $DATASIZE = 9968;
+# BBC - binary brik craft
+# DEFINE #################
+my $WALLSIZE = 100_000_000;
+my $BRIKSIZE = 10_000;
+my $PAYLOAD = 9_968;
 my $ECBSIZE = 32;
-
+# OBFUCSATION 0 LAYER ####
+my @briks = readline $REQfh; chomp @briks;
+my @obfuscation0 = sort @briks;
+# OBFUSCATION 1 LAYER ####
+my $wall = $#@obfucsation0; $wall =~ $wall / 10; $wall++;
+my $n = 0;
+while ($n < $wall)
+{
+  my @obfuscation$n = shift @obfuscation0(0..9)
+  
+}
+# MAP ####################
 my $g = "@nam[$i] @pat[$i] @siz[$i] @enc[$i]";
 my @k = "$g @chain";
 
