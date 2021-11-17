bb14005df5edbd9adb41c726a47260698cb613fa
Prince of Archives <rich.tijerina@gmail.com>
2021-11-16 21:14:51 -0700

    Update LS.pl

--- LS.pl
+++ LS.pl
@@ -3,6 +3,7 @@
 # LS - massive archive file listing
 # `ls -f ` over 10,000,000 = 2hr
 # LS over 10,000,000 = 1hr;
+# cuts the time in half for listing queries vs native c 'ls'
 use strict; use warnings;
 
 opendir(my $dh, $dir);
